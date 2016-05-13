8055f74f066000e1a8de25d6d110cc4afe4d5cc1
Viacheslav <limoozyn@gmail.com>
2016-05-13 16:35:45 +0300

    Initial commit

---
+++ js/controllers/BookController.js
@@ -0,0 +1,12 @@
+app.controller('BookController', ['$scope', '$routeParams','books',  function($scope, $routeParams, books) {
+  // Your code here
+ 
+  books.success(function(data){
+    //$scope.myBooks = data;
+    $scope.book = data[$routeParams.bookId];
+  });
+
+  // Using this property to create the URL in line 9 of views/book.html
+  $scope.currentBookIndex = parseInt($routeParams.bookId);
+  
+}]);
